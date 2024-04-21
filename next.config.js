8db320ea01cc59fd057d5c30a3994d1c9117b277
sanatankc <sanatan.iit@gmail.com>
2024-04-21 18:12:29 +0530

    push

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
+  webpack: (config, { dev, isServer }) => {
+    // Exclude *.spec.ts(x) files from being compiled by Next.js
+    config.module.rules.push({
+      test: /schoolData\.csv?$/,
+      use: 'ignore-loader',
+    });
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
